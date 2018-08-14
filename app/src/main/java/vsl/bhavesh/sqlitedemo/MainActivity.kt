d24e1492eaf1fa655cac2ae61c17a1658474ef6b
bhavesh <bhavesh.bece.13@gmail.com>
2018-08-15 00:24:57 +0530

    Insert data into Sqllite Database

--- app/src/main/java/vsl/bhavesh/sqlitedemo/MainActivity.kt
+++ app/src/main/java/vsl/bhavesh/sqlitedemo/MainActivity.kt
@@ -1,8 +1,11 @@
 package vsl.bhavesh.sqlitedemo
 
+import android.content.ContentValues
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,37 @@ class MainActivity : AppCompatActivity() {
 
         // Create table and database
         dBase.execSQL("create table if not exists employee(_id integer primary key autoincrement, id integer, name varchar(100), designation varchar(100), department varchar(100))" )
+
+        insert.setOnClickListener {
+
+            // contect value oject
+            var values = ContentValues()
+
+            // key and value want to insert
+            values.put("id", et1.text.toString().toInt())
+            values.put("name", et2.text.toString())
+            values.put("designation", et3.text.toString())
+            values.put("department", et4.text.toString())
+
+
+            // Insert data into Table
+            var status = dBase.insert("sqlite",null,values)
+
+            if(status==-1.toLong()){
+                Toast.makeText(this@MainActivity, "Failed to Insert", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this@MainActivity, "Inserted Successfully", Toast.LENGTH_LONG).show()
+                et1.setText(""); et2.setText(""); et3.setText(""); et4.setText("")
+            }
+
+
+
+
+
+
+
+        }
+
 
 
     } //OnCreate method
